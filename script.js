0ce539aaa535024041da6af92a208630acd307f4
chloevillainlesavre <chloevillainlesavre@gmail.com>
2025-11-03 10:42:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Merci pour votre message ! Nous vous répondrons bientôt.");
+});
+function ouvrirPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function fermerPopup(id) {
+  document.getElementById(id).style.display = "none";
+}
+function ouvrirPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function fermerPopup(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Merci pour votre message ! Nous vous répondrons bientôt.");
+});
